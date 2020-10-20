d4029fd251e0b8455e19f5fa5a58c5bf1d4316bf
ssr <sheetalsinghnew@gmail.com>
2020-10-21 02:52:32 +0530

    add habit left

--- src/components/inputbox.js
+++ src/components/inputbox.js
@@ -2,7 +2,7 @@ import React,{useState } from 'react';
 import './inputbox.css';
 import Progresschart from './progress_chart';
 function Inputbox(){
-    const [habitname,setHabitname] = useState();
+    const [habitname,setHabitname] = useState("");
     const [addhabit,setAddhabit] = useState(false);
   
     const addhabitfunc = () => {
@@ -12,13 +12,14 @@ function Inputbox(){
         return(
             <>
             <div className="box">
-            <label className="label">Habit name</label><br/>
+            <label className="label" onChange= { (e)=> setHabitname(habitname.push(e.target.value))}>Habit name</label><br/>
             <input className="input" ></input><br/>
-            <label class="label">Timeline(Days)</label><br/>
+            <label className="label">Timeline(Days)</label><br/>
             <input className="input"></input><br/>
             <button className="button" onClick={addhabitfunc}>ADD</button>
-            {addhabit && <Progresschart />}
+           
             </div>
+            {addhabit && <Progresschart />}
             </>
         )}
 
--- src/components/progress_chart.js
+++ src/components/progress_chart.js
@@ -2,13 +2,20 @@ import React, { useState } from 'react';
 import './progresschart.css';
 function Progresschart(){
         const [count,setCount] = useState(0);
+        var add=[];
+         for(var i=0;i<count;i++)
+            {
+                add.push(<li data-level="2"></li>); 
+            }
+          
+    
         return(
             <>
             <div className="box">
             <label className="label">Done for today?</label>
             <button className="button" onClick={() => setCount(count+1)}>Oh yes!!</button><br/>
             <h4 className="label">habit</h4>
-            <div class="graph_box">
+            <div className="graph_box">
              <div className="graph">
              <ul className="months">
              <li>Jan</li>
@@ -35,7 +42,7 @@ function Progresschart(){
             <li>Sat</li>
             </ul>
             <ul className="squares">
-            
+                {add.map(row => row)}
             </ul>
         </div>
         </div>
