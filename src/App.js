1bd45057ae41c315e4220f82445f940cb7a028e3
ssr <sheetalsinghnew@gmail.com>
2020-12-24 00:53:39 +0530

    reaviz added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Profile from './components/profile';
+import Progresschart from './components/progress_chart';
 
 
 function App() {
   return ( 
     <div className="App"> 
     <Profile/> 
+    <Progresschart/>
     </div>
     
   );
